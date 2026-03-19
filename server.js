39b3c267e5a5d6b777bc38136ab2b76810b13e87
sahuarpita322-ship-it <sahuarpita322@gmail.com>
2026-03-19 23:12:32 +0530

    sahayata

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function getLocalIP() {
   return 'localhost';
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const LOCAL_IP = getLocalIP();
 
 // Serve static files
